b7e037e775de5bcf3da8a92397f71623e655e656
yeana07 <lee.ye.naaa@gmail.com>
2025-09-20 21:15:42 +0900

    접근 불가 페이지

--- spring-auth/src/main/java/com/sparta/springauth/controller/ProductController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
+@EnableMethodSecurity(securedEnabled = true)
 public class ProductController {
 
     @GetMapping("/products")
@@ -17,6 +21,17 @@ public class ProductController {
         // Authentication 의 Principle
         User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());;
+
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
